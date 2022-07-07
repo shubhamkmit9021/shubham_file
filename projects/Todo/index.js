7eb6bc35833b7739346f5af0e164cc6e440028a5
shubhamkmit9021 <shubhamkmit9021@gmail.com>
2022-07-07 14:43:24 +0530

    todo app

---
+++ projects/Todo/index.js
@@ -0,0 +1,42 @@
+
+document.querySelector("form").addEventListener("submit",createTodo);
+
+function createTodo() {
+    event.preventDefault();
+    var task=document.querySelector("#task").value;
+    var priority=document.querySelector("#priority").value;
+    console.log(task,priority);  // used for just console
+
+    // now create table
+
+    var tr=document.createElement("tr");   // for first row
+        var td1=document.createElement("td");  // for first row of column 1  i.e task
+        td1.innerText=task;
+
+        var td2=document.createElement("td");  // for first row of column 2  i.e priority
+        td2.innerText=priority;
+            if(priority=="High")
+                td2.style.backgroundColor="red";
+            else
+                td2.style.backgroundColor="green";
+
+        var td3=document.createElement("td");   // for first row of column 3  i.e Delete
+        td3.innerText="Delete";
+        td3.style.color="red";
+        td3.style.cursor="pointer";
+        td3.addEventListener("click",deleteRow);
+
+        // for append from table body
+        tr.append(td1,td2,td3);
+        document.querySelector("tbody").append(tr);
+
+
+}
+
+function deleteRow() {
+    console.log(event.target.parentNode);  // for console
+    event.target.parentNode.remove();
+    // alert("Are You Sure !!");
+}
+
+
